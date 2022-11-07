0fd2c53b335e71ef2bf03330dcec171eca3af550
Gabriele Pollak Santos <gabrielepollak@outlook.com>
2022-11-07 09:06:21 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/CEPController.cs
@@ -0,0 +1,39 @@
+﻿using Estudoconsumoapi.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Estudoconsumoapi.Controllers
+{
+    [ApiController]
+    [Route("Controller")]
+    public class CEPController : ControllerBase
+    {
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        [HttpGet]
+        public async Task<ActionResult<Cep>> Cep(string cep)
+        {
+
+
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync("https://viacep.com.br/ws/" + cep + "/json/");
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Cep endereco = JsonSerializer.Deserialize<Cep>(responseBody);
+                return Ok(endereco);
+
+            }
+            catch (HttpRequestException e)
+            {
+                return BadRequest(e.Message);
+            }
+
+
+
+        }
+    }
+}
